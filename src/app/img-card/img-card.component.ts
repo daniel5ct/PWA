93accfd3d404ee797346e7626103f3324fbb0cde
Daniel Soares <daniel.soares@smartmed.world>
2019-01-09 17:44:45 +0000

    PWA with service worker and offline mode

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatImage } from '../Models/CatImage';
+import { Button } from '../Models/Button';
 
 @Component({
   selector: 'app-img-card',
@@ -14,13 +15,21 @@ export class ImgCardComponent implements OnInit {
   };
 
   public src: string;
+  public button: Button = {
+    text: 'Give me another cat',
+    color: 'primary',
+    disabled: false
+  };
 
   ngOnInit() {
    this.generateSrc();
   }
 
   public generateSrc(): void {
-    this.src = this.image.api + this.image.message +
-    '?size=' + this.image.fontsize + '&ts=' + Date.now();
+    this.src = this.image.api + this.image.message + '?size=' + this.image.fontsize;
+    if (!navigator.onLine) {
+      this.button.text = 'Sorry, you\'re offline';
+      this.button.disabled = true;
+    }
   }
 }
